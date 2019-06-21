c171c3631865a5d0cdbc4a087dfee808ee97a8e7
jirwincline <jakobinwins@gmail.com>
2019-06-21 10:01:24 -0700

    create function shells and a button. also a constructor to hold multiple pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 //----------------------User Interface Logic-------------------------//
+//constructors
+function Order() {
+  this.pizzas = [];
+}
 
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+// above is for ordering multiple pizzas. worry about that last.
 function Pizza() {
   this.size = size;
   this.toppings = [];
@@ -13,9 +21,25 @@ function Topping() {
 Pizza.prototype.addTopping = function(topping) {
   this.toppings.push(topping)
 }
+//functions
+function startPizza() {
+  return pizza = New Pizza
+  // will need to add to order
+}
+function addToppingChoice() {
+
+}
+function calculatePrice() {
+
+}
+function displayPurchase() {
+
+}
 
 //----------------------User Interface Logic-------------------------//
 
 $(document).ready(function(){
-
-})
+  $('#startPizzaBtn').click(function() {
+    startPizza();
+  });
+});
